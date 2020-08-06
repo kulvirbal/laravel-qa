0e38dc7746652a60738731ec4d4eb853cf36c65f
kulvir kaur <kulvirkaur@kulvirs-MacBook-Air.local>
2020-08-05 22:54:28 -0400

    add votes. answer and views count to question item

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,6 +10,17 @@
                 <div class="card-body">
                     @foreach($questions as $question)
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="votes">
+                                    <strong> {{ $question->votes }}</strong> {{ Illuminate\Support\Str::plural('vote', $question->votes) }}
+                                </div>
+                                <div class="status {{ $question->status }}">
+                                    <strong> {{ $question->answers }}</strong> {{ Illuminate\Support\Str::plural('answers', $question->answers) }}
+                                </div>
+                                <div class="view">
+                                    {{ $question->views ." ". Illuminate\Support\Str::plural('views', $question->views) }}
+                                </div>
+                            </div>
                             <div class="media-body">
                                 <h3 class="mt-0">
                                     <a href="{{ $question->user->url }}">
